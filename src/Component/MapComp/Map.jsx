b245c012f5aeb6048b3396896237cd0e8e50fcc9
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-24 13:03:02 +0530

    Added map component

---
+++ src/Component/MapComp/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../MapComp/map.css'
+
+const Map = () => {
+  return (
+    <div>
+     <div className='mapImg'>
+     <p>Temperature-data</p>
+    <img src="https://tile.openweathermap.org/map/temp_new/2/2/1.png?appid=fbfce8ba1bedd6e47a51541f12cee44b" alt="" />
+   </div> 
+    </div>
+  )
+}
+
+export default Map
